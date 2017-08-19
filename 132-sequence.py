4462f1fd690b4c0481b80035d472e8e1b291b2d6
ankittendulkar92 <ankittendulkar92@gmail.com>
2017-08-18 23:32:40 -0400

    Update 132-sequence.py

--- 132-sequence.py
+++ 132-sequence.py
@@ -1,26 +1,31 @@
 
-# coding: utf-8
-
-# In[2]:
-
-def seq_find(a):
-    found=False
-    small=float('Inf')
-    large=-float('Inf')
-    medium=0
-    for i in a:
-        if i<small:
-            small=i
-        elif i>large:
-            large=i
-        else:
-            found=True
-            break
-    return found
-
-li=[1,2,3,5,4,6,7]
-
-seq_find(li)
+def find132pattern(self, nums):
+        a=nums
+        found=False
+        small=float('Inf')
+        large=-float('Inf')
+        global n
+        for i in range(len(a)-1):
+            n=i
+            if a[i]<small:
+                small=a[i]
+            elif a[i]>large:
+                large=a[i]
+            if small<large:
+                
+                break
+        s=[-float('Inf')]
         
-    
+        for j in range(n+1,len(a)-1):
+            if a[j]<large and a[j]>small:
+                found=True
+                break
+            elif a[j]<s[-1]:
+                s.pop
+                s.append(a[j])
+            else:
+                small=s.pop
+                large=a[j]
+        
+        return found
 
