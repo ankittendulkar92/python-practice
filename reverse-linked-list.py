f91eb73e8e1bab5094deeb956c36419ca7390a9e
ankittendulkar92 <ankittendulkar92@gmail.com>
2017-09-14 20:43:48 -0400

    Update reverse-linked-list.py

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -2,3 +2,14 @@ class listnode:
   def__init__self(self,x):
     self.val=x
     self.next=None
+  def check_reverse(self,head):
+    if head==None or head.next==None:
+      return head
+    else:
+      temp=None,Next_head=None
+      while head!=None:
+        next_head=head.next
+        head.next=temp
+        temp=head
+        head=next_head
+      return temp
